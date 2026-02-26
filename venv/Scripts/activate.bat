455cd3c19eabea7a15c3cea5c07ec9ab760d17a8
Shebin K Babu <kshebin86@example.com>
2026-02-26 14:48:07 +0530

    Add requirements

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\iamsh\OneDrive\Desktop\blood bank managment\venv"
+set "VIRTUAL_ENV=D:\cyber security\blood\blood-bank-system--ben\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
